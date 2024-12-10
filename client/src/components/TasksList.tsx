0fbfdb672897e5b54d3d0327bc7314a7cdac4dc0
ola.goldin <ola.goldin@finonex.com>
2024-12-10 15:27:18 +0200

    date fix

--- client/src/components/TasksList.tsx
+++ client/src/components/TasksList.tsx
@@ -8,23 +8,28 @@ interface TaskProps {
     _id: string;
     done: boolean;
     task: string;
+    createdAt: Date;
   }>;
 }
 
 const TaskList: React.FC<TaskProps> = ({ tasks }) => {
   const dispatch = useDispatch();
 
-  
   if (!tasks || tasks.length === 0) {
     return <h2>No Records</h2>;
   }
 
   return (
     <>
-      {tasks.map(({ _id, done, task }) => (
+      {tasks.map(({ _id, done, task, createdAt }) => (
         <div key={_id} className="task">
           <div className="checkbox" onClick={() => dispatch(updateTask(_id))}>
-            <i className={`uil ${done ? "uil-check-circle" : "uil-circle"}`}></i>
+            <i className={`uil ${done ? "uil-check-circle" : "uil-circle"}`}>
+              <div>
+                <p className="title">{"Created: "}</p>
+                <p className="title"> {new Date(createdAt)?.toLocaleDateString()}</p>
+              </div>
+            </i>
             <p className={done ? "line_through" : ""}>{task}</p>
           </div>
           <i
